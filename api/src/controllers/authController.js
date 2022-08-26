5549b80c4d408996b88a367a76dc94e4f0f229e5
Craig Dempsey <craigdempsey@Craigs-MacBook-Pro.local>
2022-08-26 17:01:20 -0400

    connect express be with proxy, correct api req, style onboarding ux a bit, focustrap views

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -5,14 +5,17 @@ const User = require('../models/User');
 
 exports.login = function (req,res){
     if (res.locals.details) {
-        User.findOne({email: res.locals.details[0].toString()}, function (err,user) {
+        try {
+            User.findOne({email: res.locals.details[0].toString()}, function (err,user) {
             if (err) {
+                console.log(`some mongoose err, ${err}`);
                 return res.status(401).json({message: 'Invalid Password/Email 0'});
             }
             else if (user) {
                 console.log('user', user);
                 bcrypt.compare(res.locals.details[1], user.pw_hash.toString(), (err, isMatch) => {
                     if (err) {
+                        console.log(`err from bcrypt`);
                         return res.status(401).json({message: 'Invalid Password/Email 1'});
                     } else if (isMatch) {
                         var token = jwt.sign({user_id: user._id}, process.env.TOKEN_KEY);
@@ -23,34 +26,41 @@ exports.login = function (req,res){
                             token: token,
                         });
                     } else {
+                        console.log(`bad pw`);
                         return res.status(401).json({message: 'Invalid Password/Email 2'});
                     }
                 });
             } else { 
-                return res.status(401).json({message: 'Invalid Password/Email 3'});
+                console.log(`no user, no err?`);
+                res.status(401).json({message: 'Invalid Password/Email 3'})
+                return res;
             }
         })
-        // .catch((err) in mongose piece
+        } catch (error) {
+            console.log(`findOne error--> ${error}`);
+            return res.status(401).json({message: 'Invalid Password/Email 4'});
+        }
     } else {
+        console.log(`no details`);
         return res.status(400).json({message:'No Auth Details'});
     }
 };
 
 // var passport = require('passport');
 
-exports.fb_login = function(req,res) {
-    console.log(req.body, 'success');
-    return res.status(200).json(req.body.user)
-} 
+// exports.fb_login = function(req,res) {
+//     console.log(req.body, 'success');
+//     return res.status(200).json(req.body.user)
+// } 
 
-exports.fb_fail = function(req,res) {
-    console.log(req.body, 'fail');
-    return res.status(401).json({message: 'fb failed login'})
-}
+// exports.fb_fail = function(req,res) {
+//     console.log(req.body, 'fail');
+//     return res.status(401).json({message: 'fb failed login'})
+// }
 
-exports.fb_logout = function(req,res, next) {
-    req.logout(function (err) {
-        if (err) { return next(err); }
-        return res.status(402).json({message: 'fb logged out'})
-    })
-}
+// exports.fb_logout = function(req,res, next) {
+//     req.logout(function (err) {
+//         if (err) { return next(err); }
+//         return res.status(402).json({message: 'fb logged out'})
+//     })
+// }
--- api/src/routes/authRouter.js
+++ api/src/routes/authRouter.js
@@ -5,8 +5,8 @@ const authController = require('../controllers/authController');
 const authRouter = express.Router();
 
 authRouter.post('/login', authController.login);
-authRouter.get('/fb/login', authController.fb_login);
-authRouter.get('/fb/fail', authController.fb_fail);
-authRouter.get('/fb/logout', authController.fb_logout);
+// authRouter.get('/fb/login', authController.fb_login);
+// authRouter.get('/fb/fail', authController.fb_fail);
+// authRouter.get('/fb/logout', authController.fb_logout);
 
 module.exports = authRouter;
