be1378e848538f45a8ee9722653dcee7c6249814
Craig Dempsey <craigdempsey@Craigs-MacBook-Pro.local>
2022-10-19 15:44:02 -0400

    clean and standardize code in controllers, server index

--- api/src/controllers/moviesController.js
+++ api/src/controllers/moviesController.js
@@ -1,100 +1,97 @@
-const Movie = require('../models/Movie');
+const { Error } = require("mongoose");
+const GameMovie = require("../models/GameMovie");
 
-
-exports.index = function(req,res) {
-    /// if req params ... filter
-    Movie.find({},
-        (err,docs) => {
-            let results = ['no results'];
-            if (err) {
-                res.send(err);
-            } else if (docs.length>0) {            
-                results = docs.map((doc,i)=>{ return {
-                    // or do I need whole doc?
-                    _id: doc._id
-                }
-                });
-            }
-            res.send(results)
-        }
-	);
-};
-
-exports.show = function(req,res) {
-    console.log('req', req.params)
-    Movie.findById(req.params.id, function(err,doc) {
-        if (err) {
-            return res.send(err);
-        } else {
-            return res.json(doc);
-        }
-    });
-};
-
-exports.create = function(req, res) {
-    let params = req.body;
-    params.created_by = res.locals.user._id;
-    let movie = new Movie(params);
-    movie.save(function (err, doc) {
-        if (err) {
-            return res.send(err);
-        } else {
-            return res.json(doc);
-        }
-    });
- };
-
- exports.update = function(req, res) {
-	
-    let params = req.body;
-	if (req.body.identity){
-        params.last_name = req.body.identity[1].value;
-        params.first_name = req.body.identity[0].value;
+exports.index = async function (req, res) {
+  try {
+    const docs = await GameMovie.find({}).exec();
+    let results = ["no results"];
+    if (docs.length) {
+      results = docs;
     }
-    
-    Movie.findOneAndUpdate({"_id": req.params.id},params,
-        {new: true},
-        (err,doc) => {
-            if (err) {
-                res.send(err);
-            } else {
-                res.json(doc);
-            }
-        }
-    );
- };
-
- exports.delete = (req, res) => {
-     console.log(req.params, 'params');
-     Movie.findOneAndDelete({"_id": req.params.id},
-     (err,doc) => {
-        if (err) {
-            res.send(err);
-        } else {
-            let response = doc._id;
-            res.send(response);
-        }
-     });
- };
-
- exports.search = function(req,res) {
-	let query = req.body;
-	console.log(query, 'search');
-    Movie.find({$text: {$search: query}})
-    //    .skip(20)
-       .limit(10)
-       .exec((err,docs) => {
-		let results = ['no results'];
-		if (err) {
-			res.send(err);
-		} else if (docs.length>0) {            
-			results = docs
-		}
-		res.send(results)
-	});
+    return res.json(results);
+  } catch (error) {
+    res.status(400).json({ message: `${error.message}` });
+  }
 };
 
-exports.store = function(req,res) {
-    let query = req.body;
-    Movies
-}
+exports.show = async function (req, res) {
+  const { id } = req.params;
+  try {
+    const doc = await GameMovie.findById(id).exec();
+    if (doc) {
+      return res.json(doc);
+    } else {
+      throw new Error("error in movie show");
+    }
+  } catch (error) {
+    res.status(400).json({ message: `${error.message}` });
+  }
+};
+
+exports.create = async function (req, res) {
+  try {
+    const params = req.body.gameMovie;
+    const doc = await GameMovie.findOneAndUpdate(
+      { tmdb_id: params.tmdb_id },
+      { $set: params },
+      {
+        new: true, // return updated doc
+        upsert: true, // insert the document if it does not exist
+      }
+    ).exec();
+    if (doc) {
+      return res.json(doc);
+    } else {
+      throw new Error("error in movie create");
+    }
+  } catch (error) {
+    res.status(400).json({ message: `${error.message}` });
+  }
+};
+
+exports.update = async function (req, res) {
+  try {
+    const params = req.body;
+    const doc = await GameMovie.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: params },
+      { new: true }
+    ).exec();
+    if (doc) {
+      return res.json(doc);
+    } else {
+      throw new Error("error in movie update");
+    }
+  } catch (error) {
+    res.status(400).json({ message: `${error.message}` });
+  }
+};
+
+exports.delete = async function (req, res) {
+  try {
+    const doc = await GameMovie.findOneAndDelete({ _id: req.params.id }).exec();
+    if (doc) {
+      return res.json({ message: `movie deleted ${doc._id}` });
+    } else {
+      throw new Error("error in movie delete");
+    }
+  } catch (error) {
+    res.status(400).json({ message: `${error.message}` });
+  }
+};
+
+exports.search = async function (req, res) {
+  try {
+    const { query } = req.body;
+    let results = ["no results"];
+    const docs = await GameMovie.find({ $text: { $search: query } })
+      .limit(10)
+      .exec();
+    if (docs.length) {
+      results = docs;
+    }
+    return res.json(results);
+  } catch (error) {
+    res.status(400).json({ message: `${error.message}` });
+  }
+};
--- api/src/controllers/usersController.js
+++ api/src/controllers/usersController.js
@@ -1,75 +1,107 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const { Error } = require("mongoose");
 const saltRounds = 10;
-const User = require('../models/User');
+const User = require("../models/User");
 
-exports.show = function(req,res) {
-    if (res.locals.user) {
-        User.findById(res.locals.user._id, function(error,response) {
-            if (error) {
-                return res.send(error);
-            } else {
-                response.pw_hash = 'secured';
-                console.log(response);
-                return res.json(response);
-            }
-        });
-    } else {
-        res.status(400).json({message: 'Invalid Password/Email'});
+exports.show = async function (req, res) {
+  const _id = res.locals.user._id;
+  try {
+    if (!_id) {
+      throw new Error("bad request");
     }
-}
+    const response = await User.findById(_id).exec();
+    if (response) {
+      response.pw_hash = "secured";
+      return res.json(response);
+    } else {
+      throw new Error("Invalid Password/Email");
+    }
+  } catch (error) {
+    console.log(error.stack);
+    res.status(400).json({ message: `${error.message}` });
+  }
+};
 
-const extractUN = (email) => {
-    return email.split('@')[0];
-}
+exports.create = async function (req, res) {
+  try {
+    const params = req.body.user;
+    const pw_hash = await bcrypt.hash(params.password, saltRounds);
+    if (pw_hash) {
+      params.pw_hash = pw_hash;
+      params.username = params.email.split("@")[0];
+      let user = new User(params);
+      const response = await user.save();
+      if (response) {
+        return res.json(response);
+      }
+    } else {
+      throw new Error("error in user create");
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: `${error.message}` });
+  }
+};
 
-exports.create = function(req, res) {
-    let params = req.body.user;
-    bcrypt.hash(params.password, saltRounds, function(error, hash) {
-        params.pw_hash = hash;
-        params.username = extractUN(params.email);
-        let user = new User(params);
-        user.save(function (error, response) {
-            if (error) {
-                return res.send(error);
-            } else {
-                console.log(response);
-                res.json(response);
-            }
-        });
-        // catch (error)
-    });
- };
-
- exports.update = function(req, res) {
-    console.log('user update', req.body);
-    bcrypt.hash(req.body.newPassword, saltRounds, function(error, hash) {
+exports.update = async function (req, res) {
+  const { newPassword, data } = req.body;
+  const { _id } = res.locals.user;
+  if (newPassword) {
+    try {
+      const pw_hash = await bcrypt.hash(newPassword, saltRounds);
+      if (pw_hash) {
         let params = req.body;
-        params.pw_hash = hash;
-        params = (({username,email,pw_hash}) => ({username,email,pw_hash}))(params)
-        User.findOneAndUpdate({"_id": res.locals.user._id},{
-            username: params.username,
-            email: params.email,
-            pw_hash: params.pw_hash
-        },
-        {new: true},
-        (error,doc) => {
-            if (error) {
-                res.send([error]);
-            } else {
-                res.send(doc);
-            }
-        });
-    });
- };
+        params.pw_hash = pw_hash;
+        const doc = await User.findOneAndUpdate(
+          { _id: _id },
+          {
+            pw_hash: params.pw_hash,
+          },
+          { new: true }
+        ).exec();
+        if (doc) {
+          return res.json(doc);
+        } else {
+          throw new Error("error in user update");
+        }
+      }
+    } catch (error) {
+      return res.status(403).json({ message: `${error.message}` });
+    }
+  } else if (data && _id) {
+    try {
+      const response = User.findOneAndUpdate(
+        { _id: _id },
+        {
+          data: data,
+        }
+      ).exec();
+      if (response) {
+        return res.json(response.data);
+      } else {
+        throw new Error("error in user update data");
+      }
+    } catch (error) {
+      return res.status(400).json({ message: `${error.message}` });
+    }
+  } else {
+    res.status(400).json({ message: "Invalid User Update" });
+  }
+};
 
- exports.delete = (req, res) => {
-     console.log('user delete');
-     User.findOneAndDelete({"_id": res.locals.user._id},
-     (error,doc) => {
-         if (error) {
-             res.send([error]);
-         } else {
-             res.send(["user deleted"]);
-         }
-     });
- };
+exports.delete = async function (req, res) {
+  console.log("user delete");
+  try {
+    const doc = await User.findOneAndDelete({
+      _id: res.locals.user._id,
+    }).exec();
+    if (doc) {
+      return res.json({ message: "User Deleted" });
+    } else {
+      throw new Error("error in user delete");
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({ message: `${error.message}` });
+  }
+};
